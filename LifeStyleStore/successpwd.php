e9eb5bccc8da14578b39dfe93a57fb3b37e5d5a5
Harshitha J <naikharshitha77@gmail.com>
2018-10-16 15:43:41 +0530

    Add files via upload
    LifeStyle Store

---
+++ LifeStyleStore/successpwd.php
@@ -0,0 +1,29 @@
+<?php
+require_once("includes/common.php");
+if (!isset($_SESSION['email'])) {
+    header('location: index.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>SuccessPassword | Life Style Store</title>
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <?php include 'includes/header.php'; ?>
+        <div class="container-fluid" id="content">
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-4" id="settings-container">
+                   <h3 style = "color :green">Password changed successfully!!</h3>
+                </div>
+            </div>
+        </div>
+        <?php include("includes/footer.php"); ?>
+    </body>
+</html>
